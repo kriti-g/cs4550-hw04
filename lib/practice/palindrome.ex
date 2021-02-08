c86090974e3a2d7b20543674fd05e260d3a75a45
kriti-g <kriti7000@gmail.com>
2021-02-07 19:01:25 -0500

    impl palindrome

--- lib/practice/palindrome.ex
+++ lib/practice/palindrome.ex
@@ -1,3 +1,11 @@
-defmodule Practice.Calc do
-  TODO
+defmodule Practice.Palindrome do
+  def palindrome?(str) do
+    str == reverse_string(str)
+  end
+
+  def reverse_string(str) do
+    String.to_char_list(str)
+    |> Enum.reverse
+    |> List.to_string
+  end
 end
