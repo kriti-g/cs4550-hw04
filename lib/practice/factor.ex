3f1b6720df57b72940930e1f86d7c4d9f07cd227
kriti-g <kriti7000@gmail.com>
2021-02-10 23:06:18 -0500

    Update factor.ex

--- lib/practice/factor.ex
+++ lib/practice/factor.ex
@@ -1,10 +1,17 @@
 defmodule Practice.Factor do
-  def factor(x), do:
-    factor(x, 2, []) |> Enum.reverse
+  def factor(x) do
+    f(x, 2, [])
+    |> Enum.reverse
+  end
 
-  def factor(n, divisor, acc) when rem(n, divisor) === 0, do:
-    factor(div(n, divisor), divisor, [divisor | acc])
-  def factor(n, divisor, acc) when n > 1, do:
-    factor(n, divisor + 1, acc)
-  def factor(_, _, acc), do: acc
+  def f(x, div fs) do
+    res = div(x, div);
+    re = rem(x, div);
+    cond do
+      re === 0 -> f(res, div, [div | fs])
+      x > 1 -> f(x, div + 1, fs)
+      true -> fs
+    end
+  end
+
 end
