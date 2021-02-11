a28f9754a79e3489dcc99f18887fa76489bd229e
kriti-g <kriti7000@gmail.com>
2021-02-10 23:06:44 -0500

    Revert "Update factor.ex"
    This reverts commit 63aebbf330c7b767105549bcf47b40e789181d1b.

--- lib/practice/factor.ex
+++ lib/practice/factor.ex
@@ -5,10 +5,8 @@ defmodule Practice.Factor do
   end
 
   def f(x, div fs) do
-    res = div(x, div);
-    re = rem(x, div);
     cond do
-      re === 0 -> f(res, div, [div | fs])
+      rem(x, div) === 0 -> f(div(x, div), div, [div | fs])
       x > 1 -> f(x, div + 1, fs)
       true -> fs
     end
