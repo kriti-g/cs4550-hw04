ad5c7ddbd7f071db26958b735ef93c287c132918
kriti-g <kriti7000@gmail.com>
2021-02-10 19:20:57 -0500

    Update calc.ex

--- lib/practice/calc.ex
+++ lib/practice/calc.ex
@@ -9,16 +9,47 @@ defmodule Practice.Calc do
     # but doesn't need to handle parens.
     expr
     |> String.split(~r/\s+/)
-    |> hd
-    |> parse_float
-    |> :math.sqrt()
-
-    # Hint:
-    # expr
-    # |> split
-    # |> tag_tokens  (e.g. [+, 1] => [{:op, "+"}, {:num, 1.0}]
-    # |> convert to postfix
+    |> Enum.map(tag_token)
+    |> convert
     # |> reverse to prefix
     # |> evaluate as a stack calculator using pattern matching
+  end
+
+  def tag_token(token) do
+    if is_rator(token) do
+      {:op, token}
+    else
+      {:num, parse_float(token)}
+    end
+  end
+
+  def is_rator(token) do
+    token == "+" || token == "-" || token == "/" || token == "*"
+  end
+
+  def convert(exprlst) do
+    output = []
+    stack = []
+
+    defp process(token) do
+      if is_rator(token) do
+        if stack == [] do
+          [token | stack]
+        else
+          compari = compare(token, hd stack)
+          case compari do
+            1 -> [token | stack]
+            0 -> output = output ++ [hd stack]; stack = stack -- [hd stack]; [token | stack];
+            -1 -> output = output ++ [hd stack]; stack = stack -- [hd stack]; [token | stack]; process(token);
+          end
+        end
+      else
+        output ++ token
+      end
+    end
+
+    Enum.each(exprlst, process(token))
+    Enum.each(stack, fn(token) -> output = output ++ [hd stack]; stack = stack -- [hd stack])
+
   end
 end
